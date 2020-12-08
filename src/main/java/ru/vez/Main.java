c71e22750511768a95a5c7ac5e6e603422d01d0a
vzateychuk <vzateychuk@iqubecompany.com>
2020-12-08 20:40:45 +0300

    Печать в левом нижнем углу с печатью

--- src/main/java/ru/vez/Main.java
+++ src/main/java/ru/vez/Main.java
@@ -1,16 +1,17 @@
 package ru.vez;
 
-import com.itextpdf.io.image.ImageData;
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfName;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.kernel.pdf.annot.PdfAnnotation;
-import com.itextpdf.kernel.pdf.annot.PdfStampAnnotation;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
-import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     private static final String SRC_FILE = "/home/osboxes/tmp/page.pdf";
@@ -25,24 +26,44 @@ public class Main {
     public static void manipulatePdf(String src, String dest, String imgSrc) throws Exception {
 
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
+        PdfPage page = pdfDoc.getFirstPage();
 
-        ImageData img = ImageDataFactory.create(imgSrc);
-        float width = img.getWidth();
-        float height = img.getHeight();
-        PdfFormXObject xObj = new PdfFormXObject(new Rectangle(width, height));
-
-        PdfCanvas canvas = new PdfCanvas(xObj, pdfDoc);
-        canvas.addImageAt(img, 0, 0, false);
-
-        Rectangle location = new Rectangle(36, 770 - height, width, height);
-        PdfStampAnnotation stamp = new PdfStampAnnotation(location).setStampName(new PdfName("ITEXT"));
-        stamp.setNormalAppearance(xObj.getPdfObject());
-
-        // Set to print the annotation when the page is printed
-        stamp.setFlags(PdfAnnotation.PRINT);
-        pdfDoc.getFirstPage().addAnnotation(stamp);
+        PdfCanvas canvas = drawCanvas(page, List.of("Большой директор:", "Владимир Затейчук"));
 
         pdfDoc.close();
         canvas.release();
+    }
+
+    private static PdfCanvas drawCanvas(PdfPage page, List<String> strings) throws IOException {
+
+        PdfCanvas canvas = new PdfCanvas(page);
+        Color blueColor = ColorConstants.BLUE;
+        canvas.setLineWidth(5f).setStrokeColor(blueColor).setColor(blueColor, true);
+        final float leftX = 20f;
+        final float leftY = 20f;
+        canvas.moveTo(leftX,leftY)
+            .lineTo(150f, leftY)
+            .lineTo(150f, 80f)
+            .lineTo(leftX, 80f)
+            .lineTo(leftX, leftY).stroke();
+
+        canvas.beginText()
+            .setFontAndSize(getPdfFont(), 12)
+            .setLeading(12*1.2f)
+            .moveText(leftX+5f, leftY+50f);
+
+        for (String string : strings) {
+            canvas.newlineShowText(string);
+        }
+        return canvas.endText();
+    }
+
+    private static PdfFont getPdfFont() throws IOException {
+
+        return PdfFontFactory.createFont(
+            "/home/osboxes/tmp/freesans.otf",
+            "CP1251",
+            true
+        );
     }
 }
